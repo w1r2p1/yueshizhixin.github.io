fd0b882c08c0f537c31379c5b98ec339aa53dc82
yueshizhixin <yueshizhixin@163.com>
2018-06-07 17:46:44 +0800

    vue learn 3&4

---
+++ learn/vue/testCom1.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <span>
+            这是一个组件文件
+        </span>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "testCom1"
+    }
+</script>
+
+<style scoped>
+
+</style>
